e42326def409a09cce63471fe77d91ec12924eec
bymshihab <khaledshihab221@gmail.com>
2026-02-03 11:38:49 +0600

    add seal.js demonstrating Object.seal behavior with employee object add spread.js showcasing spread operator usage with arrays add template-string.js illustrating template literals for string interpolation

---
+++ template-string.js
@@ -0,0 +1,17 @@
+const poem = `Amm pata jora jora
+marbo chabuk chorbe ghora
+ore babu ghure dara
+asche amar pagla ghora`;
+// console.log(poem)
+
+function sum (num1, num2){
+    const result = num1 + num2;
+    // const output = 'sum of ' + num1 + ' and ' + num2 + ' is equal to ' + result;
+    const output = `sum of ${num1} and ${num2} is equal to ${result}`
+    console.log(output)
+}
+sum(5,7)
+
+const price = 50;
+const discountedPrice = `Discounted Price of ${price} after 20% discount ${price * 0.8}`
+console.log(discountedPrice)
